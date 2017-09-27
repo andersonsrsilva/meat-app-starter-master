275bb36941c3f9194f8f72d3612fcfe9cea06ec0
anderson.silva <andersonsrsilva@gmail.com>
2017-09-27 16:32:21 -0300

    Validators

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import {OrderService} from './order.service';
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
 import {Order, OrderItem} from './order.model';
 import {Router} from '@angular/router';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'mt-order',
@@ -23,6 +23,20 @@ export class OrderComponent implements OnInit {
         {label: 'Cartão de Refeição', value: 'REF'}
     ];
 
+    static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+        const email = group.get('email');
+        const emailConfirmation = group.get('emailConfirmation');
+
+        if (!email || !emailConfirmation) {
+            return undefined;
+        }
+        if (email.value !== emailConfirmation.value) {
+            return {emailsNotMatch: true};
+        }
+
+        return undefined;
+    }
+
     constructor(private orderService: OrderService,
                 private router: Router,
                 private formBuilder: FormBuilder) {
@@ -37,8 +51,10 @@ export class OrderComponent implements OnInit {
             number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
             optionalAddress: this.formBuilder.control(''),
             paymentOption: this.formBuilder.control('', [Validators.required])
-        });
+        }, {validator: OrderComponent.equalsTo});
     }
+
+
 
     itemsValues(): number {
         return this.orderService.itemsValues();
